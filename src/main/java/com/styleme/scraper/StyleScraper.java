28abc461d405a514025b84a071e7aee8d21b7d7a
evaleen <eibhlinmcg93@gmail.com>
2015-10-20 20:55:01 +0100

    Initial commit

---
+++ src/main/java/com/styleme/scraper/StyleScraper.java
@@ -0,0 +1,28 @@
+package com.styleme.scraper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class StyleScraper {
+
+    public static void main (String[] args) {
+        System.out.println("TESTED new new");
+
+        String html = "<html><head><title>First parse</title></head>"
+                + "<body><p>Parsed HTML into a doc.</p></body></html>";
+        try {
+            Document doc = Jsoup.connect("http://eu.topshop.com/en/tseu/category/clothing-485092").get();
+            Element link = doc.select("a").first();
+
+            String text = doc.html();
+            System.out.println("text:  " + text);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
